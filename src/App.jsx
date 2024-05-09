ec96df82c4d41bd82054809ec16b401b24739ec8
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-05-09 03:59:53 +0100

    mark: initial commit
    - scaffolded react project using vite and making a
      clean initial commit for the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor( props ) {
+    super( props );
+  }
+
+  render() {
+    return ( 
+      <h1>
+        hello
+      </h1>
+    )
+  }
+}
+
+export default App
