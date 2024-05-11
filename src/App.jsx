ae450b08bd8c5f423a93397993db0efaf6ca4337
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-05-11 22:39:29 +0100

    feat: completed App's markup( jsx )
    - added and completed the app's markup( i.e. the jsx ) for
      all components
    - cleaned App.css for the webapp usage/integration with the sass
      compiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import './Add_button/Add_button'
+import Add__button from './Add_button/Add_button';
+import Task from './Task/Task';
 
 class App extends React.Component {
   constructor( props ) {
@@ -8,9 +11,38 @@ class App extends React.Component {
 
   render() {
     return ( 
-      <h1>
-        hello
-      </h1>
+      <div className="app-ctn">
+        <h1 className="app-ctn__app-heading">
+          Joshua&#0027;s To-Do List {/* watch out for unicode in jsx, it defines the apostrophe */}
+        </h1>
+
+        <div className="app-ctn__app-divider"> </div>
+
+        <h2 className="app-ctn__app-sub-heading">
+          your created tasks
+        </h2>
+
+        <input type="text"className="app-ctn__search-input" />
+
+        <div className="task-list">
+
+          <Task name={"buy toothpaste"} done={ false }/>
+
+          <Task name={"buy toothpaste"} done={ false }/>
+
+          <Task name={"buy toothpaste"} done={ false }/>
+
+          <Task name={"buy toothpaste"} done={ false }/>
+
+          <Task name={"buy toothpaste"} done={ false }/>
+
+          <Task name={"buy toothpaste"} done={ false }/>
+        </div>
+
+        <div className="app-ctn__app-divider"> </div>
+
+        <Add__button/>
+      </div>
     )
   }
 }
